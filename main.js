34cc613f40e1443c5b8efdeeb54dac86b2e72b85
BiancaPavel1 <biancapavel03.bp@gmail.com>
2020-05-12 21:10:06 +0300

    finish up

--- main.js
+++ main.js
@@ -8,22 +8,24 @@ function initialize() {
     center: latlng
   }
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  callCovidApi('RO');
 }
 
 function codeAddress() {
   var address = document.getElementById('address').value;
   geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {			
-			getCovidData(results[0].address_components);
+    if (status == 'OK') {
       map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
+      getCovidData(results[0].address_components);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
+}
+
+function getCovidData(addressComponents) {
+  var countryData = getCountryData(addressComponents);
+  callCovidApi(countryData['short_name']);  
 }
 
 function getCountryData(addressComponents) {
@@ -37,23 +39,21 @@ function getCountryData(addressComponents) {
 	}	
 }
 
-function getCovidData(addressComponents) {
-	var countryData = getCountryData(addressComponents);
+function callCovidApi(countryCode) {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://covid-19-data.p.rapidapi.com/country/code?format=json&code=" + countryData['short_name'],
+    "url": "https://covid-19-data.p.rapidapi.com/country/code?format=json&code=" + countryCode,
     "method": "GET",
     "headers": {
       "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
       "x-rapidapi-key": "a152b21dacmsh7ca758470d5a2c5p135bd6jsn2c5106522991"
     }
   }
- 
   $.ajax(settings).done(function (response) {   
     updateData(response[0]);
-  });
-};
+  });  
+}
 
 function updateData(covidData) {
   console.log(covidData);
@@ -63,5 +63,3 @@ function updateData(covidData) {
   $('#critical').text(covidData.critical);
   $('#deaths').text(covidData.deaths);
 }
-
-
